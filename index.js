153fe7fdf09dbea998ab3cf064db0d098303bacb
Melanie <melanie.collins.p@gmail.com>
2024-05-10 20:05:50 -0400

    servidor conecta a db y funcionando ruta GET y POST

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import { Usuarios } from './models/usuarios.model.js'
+
+const app = express()
+
+const __dirname = import.meta.dirname
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        const usuarios = await Usuarios.findAll()
+        return res.json(usuarios)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+app.post('/usuario', async (req, res) => {
+    try {
+        const { nombre, balance } = req.body
+        const usuario = await Usuarios.create({ nombre, balance })
+        return res.json(usuario)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor encendido http://localhost:${PORT}`)
+})
